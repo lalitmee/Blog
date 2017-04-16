04b82a751bb69bd318a2d4e93262932796d97c66
lalitmee <lalitkumar.meena.lk@gmail.com>
2017-04-16 10:14:12 +0530

    Created Views for the blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
+from . import views
+
 
 urlpatterns = [
     #url(r'^posts/$', "<appname>.views.<function_name>"),
-    url(r'^$', "blog.views.post_home"),
+    url(r'^$', "blog.views.post_list"),
+    url(r'^create/$', "blog.views.post_create"),
+    url(r'^detail/$', "blog.views.post_detail"),
+    url(r'^update/$', "blog.views.post_update"),
+    url(r'^delete/$', "blog.views.post_delete"),
 
 ]
