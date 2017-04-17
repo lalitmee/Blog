2cc85b8f3798e697d634a2027f3472f02f55381f
lalitmee <lalitkumar.meena.lk@gmail.com>
2017-04-18 04:30:47 +0530

    Advancing the blog

--- Blog/settings.py
+++ Blog/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pagedown',
+    'crispy_forms',
     'markdown_deux',
     'blog',
 ]
+
+CRISPY_TEMPELATE_PACK = 'bootstrap3'
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
